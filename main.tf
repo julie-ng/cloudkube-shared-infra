0123ce0bc58303fb6e86da0d726909cd757374b1
Julie Ng <me@julie.io>
2021-03-15 14:56:56 +0100

    arm: add management lock on this shared rg

--- main.tf
+++ main.tf
@@ -24,6 +24,13 @@ resource "azurerm_resource_group" "shared_rg" {
   tags     = var.default_tags
 }
 
+resource "azurerm_management_lock" "locked_rg" {
+  name       = "shared-rg-lock"
+  scope      = azurerm_resource_group.shared_rg.id
+  lock_level = "CanNotDelete"
+  notes      = "These resources are shared by many projects and demos."
+}
+
 # DNS Zone
 # --------
 
