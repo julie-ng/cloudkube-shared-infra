5e224092af58ee4136a76fa3461f602b60feaebd
Julie Ng <me@julie.io>
2021-04-25 10:26:11 +0200

    acr - revert back to basic sku

--- main.tf
+++ main.tf
@@ -85,6 +85,7 @@ resource "azurerm_container_registry" "acr" {
   resource_group_name = azurerm_resource_group.shared_rg.name
   location            = azurerm_resource_group.shared_rg.location
   sku                 = var.azure_container_registry_sku
+  network_rule_set    = [] # Temporarily revert back to Basic SKU
   admin_enabled       = false
   tags                = var.default_tags
 }
