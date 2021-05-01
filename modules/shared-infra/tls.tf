bae17b3a831ac8873b301b9bb930b31a2a157818
Julie Ng <me@julie.io>
2021-05-01 18:24:32 +0200

    key-vaults: only need Key Vault Secrets User role

--- modules/shared-infra/tls.tf
+++ modules/shared-infra/tls.tf
@@ -86,22 +86,12 @@ resource "azurerm_key_vault_certificate" "tls_wildcard_certs" {
 #  Role Assignments
 # ==================
 
-# Reference manage identity
 data "azurerm_user_assigned_identity" "ingress_managed_ids" {
   for_each            = var.ingress_configs
   name                = each.value.ingress_user_mi_name
   resource_group_name = each.value.ingress_user_mi_rg
 }
 
-# Read access to key vault metadata
-resource "azurerm_role_assignment" "ingress_mi_kv_readers" {
-  for_each             = var.ingress_configs
-  scope                = azurerm_key_vault.vaults[each.key].id
-  role_definition_name = "Key Vault Reader"
-  principal_id         = data.azurerm_user_assigned_identity.ingress_managed_ids[each.key].principal_id
-}
-
-# Access to secrets
 resource "azurerm_role_assignment" "ingress_mi_kv_secrets_users" {
   for_each             = var.ingress_configs
   scope                = azurerm_key_vault.vaults[each.key].id
