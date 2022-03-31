ee90058032c29464ea4863b39ed0b14346dbe514
Julie Ng <me@julie.io>
2022-03-31 19:24:01 +0200

    wip

--- devops.tf
+++ devops.tf
@@ -19,16 +19,44 @@ resource "azurerm_key_vault" "devops" {
   enable_rbac_authorization  = var.key_vault_enable_rbac_authorization
 }
 
+resource "azurerm_key_vault" "devops-staging" {
+  name                       = "${var.base_name}-devops-staging-kv"
+  location                   = var.location
+  sku_name                   = var.key_vault_sku
+  resource_group_name        = var.shared_rg_name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days = var.key_vault_soft_delete_retention_days
+  purge_protection_enabled   = var.key_vault_purge_protection_enabled
+  tags                       = var.default_tags
+  enable_rbac_authorization  = var.key_vault_enable_rbac_authorization
+}
+
 # Key Vault Reader
 # ----------------
 
 # Create KV Reader for Azure DevOps Key Vault Integration
 
+// dev
 resource "azuread_application" "ci_kv_reader" {
   display_name = "${var.base_name}-ci-kv-reader-sp"
   owners       = [data.azuread_client_config.current.object_id]
 }
+
+// staging
+resource "azuread_application" "ci_staging_kv_reader" {
+  display_name = "${var.base_name}-ci-staging-kv-reader-sp"
+  owners       = [data.azuread_client_config.current.object_id]
+}
+
+// dev
 resource "azuread_service_principal" "ci_kv_reader" {
+  application_id               = azuread_application.ci_kv_reader.application_id
+  app_role_assignment_required = false
+  owners                       = [data.azuread_client_config.current.object_id]
+}
+
+// staging
+resource "azuread_service_principal" "ci_staging_kv_reader" {
   application_id               = azuread_application.ci_kv_reader.application_id
   app_role_assignment_required = false
   owners                       = [data.azuread_client_config.current.object_id]
@@ -36,8 +64,14 @@ resource "azuread_service_principal" "ci_kv_reader" {
 
 # Now store this KV readers client id & secret in Key Vault so we can access it
 
+// dev
 resource "azuread_service_principal_password" "ci_kv_reader" {
   service_principal_id = azuread_service_principal.ci_kv_reader.object_id
+}
+
+// staging
+resource "azuread_service_principal_password" "ci_staging_kv_reader" {
+  service_principal_id = azuread_service_principal.ci_staging_kv_reader.object_id
 }
 
 resource "azurerm_key_vault_secret" "ci_kv_reader_client_id" {
@@ -51,6 +85,19 @@ resource "azurerm_key_vault_secret" "ci_kv_reader_client_secret" {
   value        = azuread_service_principal_password.ci_kv_reader.value
   key_vault_id = azurerm_key_vault.devops.id
 }
+
+resource "azurerm_key_vault_secret" "ci_staging_kv_reader_client_id" {
+  name         = "${var.base_name}-ci-staging-kv-reader-client-id"
+  value        = azuread_service_principal.ci_kv_reader.application_id
+  key_vault_id = azurerm_key_vault.devops.id
+}
+
+resource "azurerm_key_vault_secret" "ci_staging_kv_reader_client_secret" {
+  name         = "${var.base_name}-ci-staging-kv-reader-client-secret"
+  value        = azuread_service_principal_password.ci_kv_reader.value
+  key_vault_id = azurerm_key_vault.devops.id
+}
+
 
 
 # ==================
