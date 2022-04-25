827248b2f832619bb2ee303b529752a14398d228
Julie Ng <me@julie.io>
2022-04-25 20:51:50 +0200

    remove azure devops specific key vault b/c using federated identity instead

--- outputs.tf
+++ outputs.tf
@@ -14,13 +14,6 @@ output "key_vaults" {
   value = module.cloudkube.key_vaults
 }
 
-output "devops_key_vault" {
-  value = {
-    name      = azurerm_key_vault.devops.name
-    vault_uri = azurerm_key_vault.devops.vault_uri
-  }
-}
-
 output "tls_certificates" {
   value = module.cloudkube.tls_certificates
 }
