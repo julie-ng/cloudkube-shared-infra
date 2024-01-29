7d20e9ec6582fc763ba6bfe38deb46e2226c39db
Julie Ng <me@julie.io>
2024-01-29 18:54:22 +0100

    chore: merge terraform files

--- main.tf
+++ main.tf
@@ -4,6 +4,22 @@
 
 data "azurerm_client_config" "current" {}
 
+# ========================
+#  AKS Clusters Resources
+# ========================
+
+# Static IPs
+
+data "azurerm_public_ip" "dev" {
+  name                = "cloudkube-dev-ingress-ip"
+  resource_group_name = "cloudkube-dev-${var.dev_suffix}-rg"
+}
+
+data "azurerm_public_ip" "staging" {
+  name                = "cloudkube-staging-ingress-ip"
+  resource_group_name = "cloudkube-staging-${var.staging_suffix}-rg"
+}
+
 # ==============
 #  Shared Infra
 # ==============
