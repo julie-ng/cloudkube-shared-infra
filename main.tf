6b73fef44b04b40ab90d48a4c4085d7535c5f518
Julie Ng <me@julie.io>
2024-04-25 15:19:03 +0200

    certs: update to 2024/5

--- main.tf
+++ main.tf
@@ -58,34 +58,34 @@ module "cloudkube" {
     dev = {
       root = {
         name      = "dev-cloudkube"
-        cert_path = "./certs/2023/combined_dev_cloudkube_io.pem"
+        cert_path = "./certs/2024/combined_dev_cloudkube_io.pem"
       }
       wildcard = {
         name      = "wildcard-dev-cloudkube"
-        cert_path = "./certs/2023/combined_star_dev_cloudkube_io.pem"
+        cert_path = "./certs/2024/combined_star_dev_cloudkube_io.pem"
       }
     }
 
     staging = {
       root = {
         name      = "staging-cloudkube"
-        cert_path = "./certs/2023/combined_staging_cloudkube_io.pem"
+        cert_path = "./certs/2024/combined_staging_cloudkube_io.pem"
       }
 
       wildcard = {
         name      = "wildcard-staging-cloudkube"
-        cert_path = "./certs/2023/combined_star_staging_cloudkube_io.pem"
+        cert_path = "./certs/2024/combined_star_staging_cloudkube_io.pem"
       }
     }
 
     prod = {
       root = {
         name      = "cloudkube"
-        cert_path = "./certs/2023/combined_cloudkube_io.pem"
+        cert_path = "./certs/2024/combined_cloudkube_io.pem"
       }
       wildcard = {
         name      = "wildcard-cloudkube"
-        cert_path = "./certs/2023/combined_star_cloudkube_io.pem"
+        cert_path = "./certs/2024/combined_star_cloudkube_io.pem"
       }
     }
   }
